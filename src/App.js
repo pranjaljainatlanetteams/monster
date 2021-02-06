6e84ad50d67f85576b429cbddddf13d23265973c
Pranjal Jain <pranjaljain.lanetteam@gmail.com>
2021-02-06 12:55:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import {render} from '@testing-library/react';
+import './card-list.component';
+import {CardList} from './card-list.component';
+import './search-box.component';
+import { SearchBox } from './search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      monsters: [],
+      searchField: '',
+      searchField1: '',
+    };
+  }
+   
+
+  componentDidMount () {
+    fetch ('https://jsonplaceholder.typicode.com/users')
+      .then (response => response.json ())
+      .then (users => this.setState ({monsters: users}));
+  }
+  handleChange=e=>{
+    this.setState({searchField:e.target.value});
+    this.setState({searchField1:e.target.value});
+  };
+  render () {
+    //concept of destructuring 
+    //pull properties of an object and set them as constant.
+
+
+    const {monsters,searchField} =this.
+    state;
+    const filteredMonster=monsters.filter(monster =>monster.name.toLowerCase().includes(searchField.toLowerCase()),monster=>monster.email.toLowerCase().includes(searchField.toLocaleLowerCase()))
+
+
+    
+
+
+    //passing flitered monster to the cardlist below.
+
+    //we are pulling searchfield value and monsters value in the const.
+
+
+    //this is equal to 
+    //const monster=this.state.monster;
+    //const searchfield =this.state.searchField;
+
+    return (
+      <div className="App">
+
+      <SearchBox
+      placeholder='search monster'
+      handleChange={e=>this.setState({searchField:e.target.value})}
+      
+       />
+       <h1>MONSTERS ROLODEX</h1>
+        < CardList monsters={filteredMonster} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
